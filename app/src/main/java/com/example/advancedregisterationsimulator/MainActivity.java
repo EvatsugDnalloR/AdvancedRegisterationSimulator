11b4620e71f10198ce217d00949d2ff256964c8f
EvatsugDnalloR <gustaverolland777@gmail.com>
2022-12-29 22:59:41 +0100

    progressing...

--- app/src/main/java/com/example/advancedregisterationsimulator/MainActivity.java
+++ app/src/main/java/com/example/advancedregisterationsimulator/MainActivity.java
@@ -7,6 +7,10 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioGroup;
 
 import com.google.android.material.snackbar.Snackbar;
 
@@ -14,6 +18,13 @@ public class MainActivity extends AppCompatActivity {
 
     private ConstraintLayout parent;
     private Button uselessButton;
+    private EditText userName;
+    private EditText userEmail;
+    private EditText userPassword;
+    private EditText reEnterPassword;
+    private CheckBox agreeBox;
+    private RadioGroup genderGroup;
+
 
 
     @SuppressLint("MissingInflatedId")
@@ -24,6 +35,15 @@ public class MainActivity extends AppCompatActivity {
 
         parent = findViewById(R.id.parentLayout);
         uselessButton = findViewById(R.id.uselessButton);
+
+        agreeBox = findViewById(R.id.agreeBox);
+
+        genderGroup = findViewById(R.id.genderGroup);
+
+        userName = findViewById(R.id.userName);
+        userEmail = findViewById(R.id.userEmail);
+        userPassword = findViewById(R.id.userPassword);
+        reEnterPassword = findViewById(R.id.reEnterPassword);
 
         uselessButton.setOnClickListener(new View.OnClickListener() {
             @Override
